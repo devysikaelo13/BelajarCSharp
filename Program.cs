7a141044d0eba27296e59db3f6b067cb9354f0f9
devysikaelo13 <138899761+devysikaelo13@users.noreply.github.com>
2024-03-07 18:28:13 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarCSharp{
+    class Program { 
+        static void Main(string[] args) {
+            Console.WriteLine("selamat datang di C#");
+            Console.WriteLine("tekan apa aja untuk keluar");
+            Console.ReadKey();
+        }
+    }
+}
